8d37990fee90538a9081f738d03184b80220bb9c
davin.bao <davin.bao@gmail.com>
2020-02-10 18:38:46 +0800

    add script

--- test.js
+++ test.js
@@ -4,7 +4,9 @@
 // https://www.totaljs.com
 // ===================================================
 
-const options = {};
+const options = {
+    port: process.env.PORT || 5000
+};
 
 // options.ip = '127.0.0.1';
 // options.port = parseInt(process.argv[2]);
